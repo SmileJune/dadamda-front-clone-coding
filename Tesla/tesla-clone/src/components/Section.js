dc42814c8d1cb6cf44090f001b69ad40e4329454
ShinYoung-Kim <lukey0515@naver.com>
2023-06-23 18:31:19 +0900

    [DAD-306] Style: Section styling

--- Tesla/tesla-clone/src/components/Section.js
+++ Tesla/tesla-clone/src/components/Section.js
@@ -4,7 +4,18 @@ import styled from 'styled-components'
 function Section() {
   return (
     <Wrap>
-        section
+        <ItemText>
+            <h1>Model S</h1>
+            <p>Order Online for Touchles Delivery</p>
+        </ItemText>
+        <ButtonGroup>
+            <LeftButton>
+                Custom Order
+            </LeftButton>
+            <RightButton>
+                Existing Inventory
+            </RightButton>
+        </ButtonGroup>
     </Wrap>
   )
 }
@@ -18,4 +29,37 @@ const Wrap = styled.div`
     background-position: center;
     background-repeat: no-repeat;
     background-image: url('/images/model-s.jpg');
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between; // 원래 horizontal 정렬 -> flex direction을 column으로 변경하면서 vertical 정렬
+    align-items: center; // 원래 vertical 정렬 -> flex direction을 column으로 변경하면서 horizontal 정렬
+`
+
+const ItemText = styled.div`
+    padding-top: 10vh;
+    text-align: center;
+`
+
+const ButtonGroup = styled.div`
+    display: flex;
+    margin-bottom: 30px;
+`
+
+const LeftButton = styled.div`
+    background-color: rgba(23, 26, 32, 0.8);
+    height: 40px;
+    width: 250px;
+    color: white;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: 100px;
+    opacity: 0.85;
+    text-transform: uppercase;
+    font-size: 12px;
+    cursor: pointer;
+`
+
+const RightButton = styled(LeftButton)`
+
 `
