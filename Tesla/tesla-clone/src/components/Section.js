16e6f39ff353894e05fc4e75f14e087afd470474
ShinYoung-Kim <lukey0515@naver.com>
2023-06-23 19:14:50 +0900

    [DAD-306] Feat: Section 추가 및 rightbutton 선택 적용

--- Tesla/tesla-clone/src/components/Section.js
+++ Tesla/tesla-clone/src/components/Section.js
@@ -13,9 +13,11 @@ function Section({ title, description, leftBtnText, rightBtnText, backgroundImg}
             <LeftButton>
                { leftBtnText } 
             </LeftButton>
-            <RightButton>
-               { rightBtnText } 
-            </RightButton>
+            { rightBtnText &&
+                <RightButton>
+                { rightBtnText } 
+                </RightButton>
+            } {/* rightBtnText가 있다면 RightButton이 보이도록  */}
         </ButtonGroup>
         <DownArrow src="/images/down-arrow.svg" />
         </Buttons>
